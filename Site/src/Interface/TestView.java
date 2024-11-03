08eaaf602cde3dae92774536ff3d60c9f1d4bc6c
John Doe <johndoe@email.com>
2024-11-04 00:12:08 +0100

    Organisation de TestView
    Decoupage en méthodes

--- Site/src/Interface/TestView.java
+++ Site/src/Interface/TestView.java
@@ -1,13 +1,16 @@
 package Interface;
 
+import java.io.IOException;
 import java.util.List;
 
 import Products.Produit;
 import database.ProduitDAO;
 import javafx.application.Application;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
@@ -29,25 +32,81 @@ import javafx.stage.Stage;
 public class TestView extends Application {
 
 	private Scene mainScene;
+	private AnchorPane root;
 	
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws Exception {
+    	//Parent root2 = FXMLLoader.load(getClass().getResource("Style3.fxml"));
+    	
     	// Barre supérieure avec nom du magasin
-
-    	// Nom du magasin à gauche
+    	initializeRootLayout();
+        afficherInitialize(primaryStage);
+        createHeader();
+        createProductSection(primaryStage);
+        createFilterBox();
+        createScrollPane();
+        
+        setupMainScene(primaryStage);
+        
+        
+        // Section du panier
+        Button panierButton = new Button("Panier");
+        // Placer le bouton panier en bas
+        AnchorPane.setBottomAnchor(panierButton, 10.0);
+        AnchorPane.setLeftAnchor(panierButton, 100.0);
+        root.getChildren().addAll(panierButton);
+  
+        
+        
+        primaryStage.show();
+    }
+    
+    /** 
+     * Création AnchorPane root
+     */
+    public void initializeRootLayout() {
+    	// Organisation de la mise en page principale
+        root = new AnchorPane();
+    }
+    
+    /**
+     * Crée l'en-tête de l'application, qui inclut le nom du magasin affiché en haut à gauche.
+     */
+    public void createHeader() {
     	Label Name = new Label("Shop");
     	Name.setStyle("-fx-text-fill: #333; -fx-font-size: 30px; -fx-font-weight: bold;");
-
-    	// Organisation de la mise en page principale
-        AnchorPane root = new AnchorPane();
-        afficherInitialize(root);
-
-        // Section principale
+    	//Placer le nom en haut à gauche
+        AnchorPane.setTopAnchor(Name, 0.0);
+        AnchorPane.setLeftAnchor(Name, 10.0);
+        root.getChildren().add(Name);
+    }
+    
+    /**
+     * Crée et place la section de produits au centre de l'interface utilisateur.
+     * 
+     * @param primaryStage La scène principale de l'application.
+     */
+    public void createProductSection(Stage primaryStage) {
         VBox sectionProduits = new VBox();
-        sectionProduits.getChildren().add(afficherProduits(primaryStage));
-
-        // Création d'un panneau latéral pour les filtres
+        sectionProduits.getChildren().add(displayProducts(primaryStage));
+        
+        AnchorPane.setTopAnchor(sectionProduits, 120.0);
+        AnchorPane.setLeftAnchor(sectionProduits, 10.0);
+        AnchorPane.setRightAnchor(sectionProduits, 200.0); // Ajuster la largeur du côté droit pour les filtres
+        
+        root.getChildren().add(sectionProduits);
+    }
+    
+    /**
+     * Crée une boîte latérale contenant des filtres de produits.
+     */
+    private void createFilterBox() {
+    	// Création d'un panneau latéral pour les filtres
         VBox filtreBox = new VBox();
+        AnchorPane.setTopAnchor(filtreBox, 120.0);
+        AnchorPane.setRightAnchor(filtreBox, 10.0);
+        
+        // Ajout des filtres de taille et de sexe
         Label filtreTaille = new Label("Tailles");
         CheckBox tailleS = new CheckBox("Taille S");
         CheckBox tailleM = new CheckBox("Taille M");
@@ -59,60 +118,54 @@ public class TestView extends Application {
         CheckBox SexeFemme = new CheckBox("Femmes");
         CheckBox Enfant = new CheckBox("Enfants");
         filtreBox.getChildren().addAll(filtreSexe, SexeHomme, SexeFemme, Enfant);
-
-        // Section du panier
-        Button panierButton = new Button("Panier");
-
-        //Placer le nom en haut à gauche
-        AnchorPane.setTopAnchor(Name, 0.0);
-        AnchorPane.setLeftAnchor(Name, 10.0);
-
-        // Placer la section des produits au centre
-        AnchorPane.setTopAnchor(sectionProduits, 120.0);
-        AnchorPane.setLeftAnchor(sectionProduits, 10.0);
-        AnchorPane.setRightAnchor(sectionProduits, 200.0); // Ajuster la largeur du côté droit pour les filtres
-
-        // Placer la boîte de filtres à droite
-        AnchorPane.setTopAnchor(filtreBox, 120.0);
-        AnchorPane.setRightAnchor(filtreBox, 10.0);
-
-        // Placer le bouton panier en bas
-        AnchorPane.setBottomAnchor(panierButton, 10.0);
-        AnchorPane.setLeftAnchor(panierButton, 100.0);
-
-        // Ajouter les éléments à l'AnchorPane
-        root.getChildren().addAll(Name, sectionProduits, filtreBox, panierButton);
         
-     // Créer le ScrollPane et y ajouter le GridPane
+        root.getChildren().add(filtreBox);
+    }
+    
+    /**
+     * Crée un conteneur défilant (ScrollPane) pour la mise en page principale, permettant le défilement du contenu.
+     */
+    public void createScrollPane() {
         ScrollPane scrollPane = new ScrollPane();
         scrollPane.setContent(root);
         scrollPane.setFitToWidth(true); // Le contenu s'adapte à la largeur de la fenêtre
         scrollPane.setPannable(true);   // Permet le défilement via la souris
-        
-        
-        // Création de la scène et affichage
+    }
+    
+    /**
+     * Configure la scène principale de l'application et applique les styles CSS.
+     * 
+     * @param primaryStage La scène principale de l'application.
+     * @throws IOException En cas d'erreur de chargement des ressources.
+     */
+    public void setupMainScene(Stage primaryStage) throws IOException{
+    	// Création de la scène et affichage
         mainScene = new Scene(root, 800, 600);
-        
         //scene.getStylesheets().add("file:/Site/resources/style.css");
         String css = this.getClass().getResource("/style.css").toExternalForm();
         mainScene.getStylesheets().add(css);
-        
         Image icon = new Image (getClass().getResource("/Image/logo.jpg").toExternalForm());
         
         primaryStage.getIcons().add(icon);
         primaryStage.setTitle("Magasin de tennis");
         primaryStage.setScene(mainScene);
         primaryStage.setFullScreen(false); //A remettre vrai
-        primaryStage.show();
     }
     
-    public void afficherInitialize(AnchorPane root) {
+    /**
+     * Affiche l'icône du compte en haut à droite de l'interface utilisateur.
+     * Ajoute également une barre de navigation avec plusieurs catégories.
+     * 
+     * @param primaryStage La scène principale de l'application.
+     */
+    public void afficherInitialize(Stage primaryStage) {
     	// Icône du compte à droite (bouton avec icône)
     	ImageView accountIcon = new ImageView(new Image(getClass().getResource("/Image/accountIcon.png").toExternalForm()));
     	accountIcon.setFitHeight(30); // Ajuster la taille de l'image
     	accountIcon.setFitWidth(30); 
-    	Button accountButton = new Button();
     	
+    	Button accountButton = new Button();
+
     	//Placer l'icone du compte en haut à droite
         AnchorPane.setTopAnchor(accountButton, 0.0);
         AnchorPane.setRightAnchor(accountButton, 10.0);
@@ -123,9 +176,16 @@ public class TestView extends Application {
     	// Changement du curseur lors du survol du produit
         accountButton.setOnMouseEntered(event -> accountButton.setCursor(javafx.scene.Cursor.HAND));
         accountButton.setOnMouseExited(event -> accountButton.setCursor(javafx.scene.Cursor.DEFAULT));
+        
+     // Gestion du clic sur le produit pour afficher les détails
+        accountButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                loginPage(primaryStage);
+            }
+        }); 	
     	
-    	
-    	// Création de la barre de navigation
+    	// Barre de menu
         MenuBar menuBar = new MenuBar();
         Menu menuVetements = new Menu("Vêtements");
         Menu menuSacs = new Menu("Sacs");
@@ -140,7 +200,13 @@ public class TestView extends Application {
         root.getChildren().addAll(accountButton, menuBar);
     }
     
-    public GridPane afficherProduits(Stage primaryStage) {
+    /**
+     * Affiche une grille de produits dans une interface utilisateur de type GridPane.
+     * 
+     * @param primaryStage La scène principale de l'application.
+     * @return Le GridPane contenant tous les produits.
+     */
+    public GridPane displayProducts(Stage primaryStage) {
         GridPane produitsGrid = new GridPane();
         produitsGrid.setPadding(new Insets(10, 10, 10, 10));
         produitsGrid.setHgap(10);
@@ -149,34 +215,13 @@ public class TestView extends Application {
         // Récupérer les produits depuis la base de données
         ProduitDAO produitDAO = new ProduitDAO();
         List<Produit> produits = produitDAO.getAllProduits();
-        
-        // Boucle pour ajouter chaque produit à la grille
         int colonne = 0;
         int ligne = 0;
         
+        // Boucle pour ajouter chaque produit à la grille
         for (Produit produit : produits) {
-            // Création des composants pour chaque produit
-            ImageView imageView = new ImageView(new Image(getClass().getResource(produit.getImagePath()).toExternalForm()));
-            imageView.setFitHeight(150);
-            imageView.setFitWidth(150);
-
-            Label nomProduit = new Label(produit.getNom());
-            nomProduit.getStyleClass().add("nom-produit"); // Appliquer la classe CSS
-            Label prixProduit = new Label("Prix : " + produit.getPrice() + "€");
-            prixProduit.getStyleClass().add("prix-produit"); // Appliquer la classe CSS
-
-            VBox produitBox = new VBox();
-            produitBox.getStyleClass().add("produit-box"); // Appliquer la classe CSS
-            produitBox.getChildren().addAll(imageView, nomProduit, prixProduit);
+        	VBox produitBox = createProductBox(primaryStage, produit);
             
-            // Gestion du clic sur le produit pour afficher les détails
-            produitBox.setOnMouseClicked(new EventHandler<MouseEvent>() {
-                @Override
-                public void handle(MouseEvent event) {
-                    afficherDetailsProduit(primaryStage, produit);
-                }
-            });
-
             // Ajouter le produit à la grille
             produitsGrid.add(produitBox, colonne, ligne);
 
@@ -187,12 +232,44 @@ public class TestView extends Application {
                 ligne++;
             }
         }
-
         return produitsGrid;
-    }
+    }   
     
- // Créer une scène de détails du produit
-    public void afficherDetailsProduit(Stage primaryStage, Produit produit) {
+    /**
+     * Crée un conteneur VBox pour afficher chaque produit avec ses informations.
+     * 
+     * @param primaryStage La scène principale de l'application.
+     * @param produit      Le produit à afficher.
+     * @return Un VBox contenant l'image, le nom et le prix du produit.
+     */
+    private VBox createProductBox(Stage primaryStage, Produit produit) {
+    	// Création des composants pour chaque produit
+        ImageView imageView = new ImageView(new Image(getClass().getResource(produit.getImagePath()).toExternalForm()));
+        imageView.setFitHeight(150);
+        imageView.setFitWidth(150);
+
+        Label nomProduit = new Label(produit.getNom());
+        nomProduit.getStyleClass().add("nom-produit"); // Appliquer la classe CSS
+        Label prixProduit = new Label("Prix : " + produit.getPrice() + "€");
+        prixProduit.getStyleClass().add("prix-produit"); // Appliquer la classe CSS
+
+        VBox produitBox = new VBox();
+        produitBox.getStyleClass().add("produit-box"); // Appliquer la classe CSS
+        produitBox.getChildren().addAll(imageView, nomProduit, prixProduit);
+        
+        // Gestion du clic sur le produit pour afficher les détails
+        produitBox.setOnMouseClicked(event -> showProductDetails(primaryStage, produit));
+        
+        return produitBox;
+     }
+    
+    /**
+     * Affiche les détails d'un produit spécifique dans une nouvelle scène.
+     * 
+     * @param primaryStage La scène principale de l'application.
+     * @param produit      Le produit dont les détails sont à afficher.
+     */
+    public void showProductDetails(Stage primaryStage, Produit produit) {
         // Création d'un nouveau conteneur pour les détails du produit
         VBox detailsBox = new VBox();
         detailsBox.setPadding(new Insets(20));
@@ -206,17 +283,20 @@ public class TestView extends Application {
         imageView.setFitHeight(200);
         imageView.setFitWidth(200);
         
-        // Ajouter les éléments à la boîte de détails
-        detailsBox.getChildren().addAll(nomLabel, prixLabel, imageView);
-        
         // Bouton pour revenir à la scène principale
         Button retourButton = new Button("Retour");
         retourButton.setOnAction(e -> primaryStage.setScene(mainScene));  // revenir à la scène principale
-        detailsBox.getChildren().add(retourButton);
+
+        // Ajouter les éléments à la boîte de détails
+        detailsBox.getChildren().addAll(nomLabel, prixLabel, imageView, retourButton);
         
         // Nouvelle scène pour les détails
         Scene detailScene = new Scene(detailsBox, 800, 600);
         primaryStage.setScene(detailScene);
+    }
+    
+    public void loginPage(Stage primaryStage) {
+    	
     }
 
     public static void main(String[] args) {
---
+++ Site/src/Products/Chaussures.java
@@ -0,0 +1,10 @@
+package Products;
+
+public class Chaussures extends Produit {
+
+	public Chaussures(int id, String Nom, String Description, double Price, int QtDispo, String Type, String imagePath) {
+		super(id, Nom, Description, Price, QtDispo, Type, imagePath);
+	}
+	
+
+}
